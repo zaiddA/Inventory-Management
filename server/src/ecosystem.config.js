5d2715be293aafde4adcd3d6092ffefc6c128a55
Mohammad Zaid Ansari <128237689+zaiddA@users.noreply.github.com>
2025-02-05 01:02:10 +0530

    Update ecosystem.config.js

--- server/src/ecosystem.config.js
+++ server/src/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports={
         {
             name:"Inventory Management System",
             script:"npm",
-            args:"run-dev",
+            args:"run dev",
             env:{
                 NODE_ENV:"development",
                 ENV_VAR1:"enivronment variable 1",
