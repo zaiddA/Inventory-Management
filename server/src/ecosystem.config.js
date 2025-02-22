ebe5e20b9d6920dabb29fcf5306ec5d616276bf3
Zaid Ansari <zaidansarimd9@gmail.com>
2025-02-22 22:07:18 +0530

    Add ESLint configuration, update PM2 settings, and add VSCode settings

--- server/src/ecosystem.config.js
+++ server/src/ecosystem.config.js
@@ -1,13 +1,14 @@
-module.exports={
-    apps:[
-        {
-            name:"Inventory Management System",
-            script:"npm",
-            args:"run-dev",
-            env:{
-                NODE_ENV:"development",
-                ENV_VAR1:"enivronment variable 1",
-            }
-        }
-    ]
-}
+// This file is used to configure the pm2 process manager
+module.exports = {
+  apps: [
+    {
+      name: "Inventory-Management",
+      script: "npm",
+      args: "run dev",
+      env: {
+        NODE_ENV: "development",
+        ENV_VAR1: "environment-variable",
+      },
+    },
+  ],
+};
