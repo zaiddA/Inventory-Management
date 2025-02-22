3f9d4cca9e41a73d5e6982d725a2f4787f8db45b
Zaid Ansari <zaidansarimd9@gmail.com>
2025-02-22 22:36:49 +0530

    Fix storeRef initialization to allow null value

--- client/src/app/resux.tsx
+++ client/src/app/resux.tsx
@@ -80,7 +80,7 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>();
+  const storeRef = useRef<AppStore | null>(null);
   if (!storeRef.current) {
     storeRef.current = makeStore();
     setupListeners(storeRef.current.dispatch);
